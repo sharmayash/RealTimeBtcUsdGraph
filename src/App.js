ce8c345fda8a68c3b078ff557fe14de764c8b8b7
yash <sharmayash44ys@gmail.com>
2020-07-15 01:12:50 +0530

    setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import React from "react"
+import Paper from "@material-ui/core/Paper"
+import { Chart, AreaSeries } from "@devexpress/dx-react-chart-material-ui"
+
+const data = [
+  { argument: 1, value: 10 },
+  { argument: 2, value: 20 },
+  { argument: 3, value: 30 },
+  { argument: 4, value: 11 },
+  { argument: 5, value: 12 },
+  { argument: 6, value: 9 },
+  { argument: 7, value: 3 },
+  { argument: 8, value: 30 },
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Paper>
+        <Chart data={data}>
+          <AreaSeries valueField="value" argumentField="argument" />
+        </Chart>
+      </Paper>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
